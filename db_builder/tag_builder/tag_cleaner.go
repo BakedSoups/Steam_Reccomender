68533c6e723cd397c52888870b350ba450198b07
BakedSoups <overbakedrice@gmail.com>
2025-05-10 14:03:33 -0700

    working on steam_appid identification

--- db_builder/tag_builder/tag_cleaner.go
+++ db_builder/tag_builder/tag_cleaner.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -8,6 +9,7 @@ import (
 	"os"
 
 	_ "github.com/mattn/go-sqlite3"
+	openai "github.com/sashabaranov/go-openai"
 )
 
 type Gametag struct {
@@ -18,22 +20,54 @@ type Gametag struct {
 	Tags    []string `json:"tags"`
 }
 
-func main() {
+func findMatches(client openai.Client) {
+	resp, err := client.CreateChatCompletion(
+		context.Background(),
+		openai.ChatCompletionRequest{
+			Model: openai.GPT3Dot5Turbo,
+			Messages: []openai.ChatCompletionMessage{
+				{
+					Role:    openai.ChatMessageRoleUser,
+					Content: "Hello!",
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		fmt.Printf("ChatCompletion error: %v\n", err)
+		return
+	}
+
+	fmt.Println(resp.Choices[0].Message.Content)
+}
+
+func gameVerdicts() ([]Gametag, error) {
 	file, err := os.Open("game_verdicts_with_tags.json")
 
 	if err != nil {
-		log.Fatal("ERROR: ", err)
+		return nil, err
 	}
 	defer file.Close()
 
 	bytes, err := io.ReadAll(file)
 	if err != nil {
-		log.Fatal("ERROR: ", err)
+		return nil, err
 	}
-
 	var gametags []Gametag
 
 	err = json.Unmarshal(bytes, &gametags)
+	if err != nil {
+		return nil, err
+	}
+
+	return gametags, nil
+
+}
+
+func main() {
+
+	gametags, err := gameVerdicts()
 	if err != nil {
 		log.Fatal("ERROR: ", err)
 	}
@@ -46,6 +80,9 @@ func main() {
 		fmt.Printf("Tags: %v\n", game.Tags)
 	}
 
-	// ok now I want to comapare any potential duplicates before data insertion
+	// ok now I want to find the steam appid assoiated with  before data insertion
+	client := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
+
+	findMatches(*client)
 
 }
