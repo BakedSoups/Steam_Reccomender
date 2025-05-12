673962f9ac246d343958f1ac1edb92a34d3742f8
BakedSoups <overbakedrice@gmail.com>
2025-05-12 00:32:33 -0700

    finally

--- db_builder/main.go
+++ db_builder/main.go
@@ -40,9 +40,20 @@ func main() {
 
 		if match != "" {
 			count += 1
-			fmt.Printf("%s matched with %s this is the appid%d\n", verdict.Name, match, appid)
+			// fmt.Printf("%s matched with %s this is the appid%d\n", verdict.Name, match, appid)
 			// GREAT we found a match we know the appid to insert now
-			add_match(dbo, appid, verdict)
+			transactIgnScores(dbo, appid, verdict)
+			for tag, ratio := range verdict.Ratio {
+				fmt.Printf("Inserted %v , %v, %v, %v\n", match, appid, tag, ratio)
+				transactIgbtag(dbo, appid, tag, ratio)
+			}
+			for _, tag := range verdict.SubjectiveTag {
+				transactSubjectivetag(dbo, appid, tag)
+			}
+
+			for _, tag := range verdict.UniqueTag {
+				transactUniquetag(dbo, appid, tag)
+			}
 		}
 
 	}
@@ -50,7 +61,7 @@ func main() {
 
 }
 
-func add_match(db *sql.DB, appid int, verdict Gametag) error {
+func transactIgnScores(db *sql.DB, appid int, verdict Gametag) error {
 	fmt.Println(verdict)
 	tx, err := db.Begin()
 	if err != nil {
@@ -58,8 +69,66 @@ func add_match(db *sql.DB, appid int, verdict Gametag) error {
 	}
 
 	_, err = tx.Exec(` 
-	INSERT INTO ign_tags(steam_appid)
-	VALUES(?,?)`, appid)
+	INSERT INTO ign_scores(steam_appid, score, genre)
+	VALUES(?,?)
+	`, appid, verdict.Score, verdict.MainGenre)
+
+	if err != nil {
+		tx.Rollback() // Rollback
+		return err
+	}
+
+	return tx.Commit()
+}
+
+func transactIgbtag(db *sql.DB, appid int, tag string, ratio int) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(` 
+	INSERT INTO ign_tags(steam_appid, tag, ratio)
+	VALUES(?,?,?)
+	`, appid, tag, ratio)
+
+	if err != nil {
+		tx.Rollback() // Rollback
+		return err
+	}
+
+	return tx.Commit()
+}
+
+func transactUniquetag(db *sql.DB, appid int, tag string) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(` 
+	INSERT INTO ign_unique_tags(steam_appid, unique_tag)
+	VALUES(?,?)
+	`, appid, tag)
+
+	if err != nil {
+		tx.Rollback() // Rollback
+		return err
+	}
+
+	return tx.Commit()
+}
+
+func transactSubjectivetag(db *sql.DB, appid int, tag string) error {
+	tx, err := db.Begin()
+	if err != nil {
+		return err
+	}
+
+	_, err = tx.Exec(` 
+	INSERT INTO ign_subjective_tags(steam_appid, subjective_tag)
+	VALUES(?,?)
+	`, appid, tag)
 
 	if err != nil {
 		tx.Rollback() // Rollback
@@ -75,7 +144,8 @@ func createIGNTable(db *sql.DB) {
 	CREATE TABLE IF NOT EXISTS ign_scores ( 
 		game_id INTEGER PRIMARY KEY AUTOINCREMENT, 
 		steam_appid INTEGER NOT NULL UNIQUE, 
-		score REAL NOT NULL
+		score REAL NOT NULL,
+		genre TEXT
 	);
 	`
 	_, err := db.Exec(ignKey)
@@ -83,12 +153,39 @@ func createIGNTable(db *sql.DB) {
 	if err != nil {
 		log.Fatal("ERROR: ", err)
 	}
-	tagTable := `
+	tag_tag_table := `
 	CREATE TABLE IF NOT EXISTS ign_tags(
-		game_tags INTERGER 
+		game_id INTEGER PRIMARY KEY AUTOINCREMENT,
+		steam_appid INTERGER NOT NULL UNIQUE,
+		tag TEXT NOT NULL,
+		ratio INTERGER NOT NULL
 	);
 	`
-	_, err = db.Exec(tagTable)
+	_, err = db.Exec(tag_tag_table)
+
+	if err != nil {
+		log.Fatal("ERROR: ", err)
+	}
+	unique_tag_table := `
+	CREATE TABLE IF NOT EXISTS ign_unique_tags(
+		game_id INTEGER PRIMARY KEY AUTOINCREMENT,
+		steam_appid INTERGER NOT NULL UNIQUE,
+		unique_tag TEXT NOT NULL
+	);
+	`
+	_, err = db.Exec(unique_tag_table)
+
+	if err != nil {
+		log.Fatal("ERROR: ", err)
+	}
+	subjective_tags_table := `
+	CREATE TABLE IF NOT EXISTS ign_subjective_tags(
+		game_id INTEGER PRIMARY KEY AUTOINCREMENT,
+		steam_appid INTERGER NOT NULL UNIQUE,
+		subjective_tag TEXT NOT NULL
+	);
+	`
+	_, err = db.Exec(subjective_tags_table)
 
 	if err != nil {
 		log.Fatal("ERROR: ", err)
